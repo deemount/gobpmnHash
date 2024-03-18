45eb2a89e956f21d4f605b056a8e419102ca0a0a
Salvatore Gonda <salvatore.gonda@gmail.com>
2024-03-18 11:59:21 +0100

    added testify and build a better unit test

--- go.mod
+++ go.mod
@@ -5,6 +5,13 @@ go 1.21.7
 require (
 	github.com/deemount/gobpmnModels v0.0.0-20240316085912-7831ff39ffa4
 	github.com/deemount/gobpmnReflection v0.0.0-20240315112013-036495e4c08d
+	github.com/stretchr/testify v1.9.0
 )
 
-require github.com/deemount/gobpmnTypes v0.0.0-20240315111519-43046016ad9f // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/deemount/gobpmnTypes v0.0.0-20240315111519-43046016ad9f // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- inject_test.go
+++ inject_test.go
@@ -3,14 +3,47 @@ package gobpmn_hash_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	gobpmn_hash "github.com/deemount/gobpmnHash"
 	"github.com/deemount/gobpmnModels/pkg/core"
 )
 
-type Injection struct {
-	Suffix string
+/*
+
+Note:
+Test Process and TestEmbeddedProcess are two structs copied from the
+gobpmnByExample package.
+
+*/
+
+// TestEmbeddedProcess is a struct that contains fields of type
+// core.DefinitionsRepository and two anonymous embedded structs.
+// It is a copy of the gobpmnByExample/example/01 package.
+type TestEmbeddedProcess struct {
+	Def core.DefinitionsRepository
+	TestEmbeddedPool
+	TestEmbeddedTenant
 }
 
+// TestEmbeddedPool is a struct that contains fields of type
+// gobpmn_hash.Injection and bool.
+// It is embedded in the TestEmbeddedProcess struct.
+type TestEmbeddedPool struct {
+	XYZIsExecutable bool
+	XYZProcess      gobpmn_hash.Injection
+}
+
+// TestEmbeddedTenant is a struct that contains a field of type
+// gobpmn_hash.Injection.
+// It is embedded in the TestEmbeddedProcess struct.
+type TestEmbeddedTenant struct {
+	XYZStartEvent gobpmn_hash.Injection
+}
+
+// TestProcess is a struct that contains fields of type
+// core.DefinitionsRepository, gobpmn_hash.Injection, and bool.
+// It is a copy of the gobpmnByExample/example/02 package.
 type TestProcess struct {
 	Def            core.DefinitionsRepository
 	IsExecutable   bool
@@ -22,51 +55,58 @@ type TestProcess struct {
 	EndEvent       gobpmn_hash.Injection
 }
 
-type TestEmbeddedProcess struct {
-	Def core.DefinitionsRepository
-	TestEmbeddedPool
-	TestEmbeddedTenant
-}
-
-type TestEmbeddedPool struct {
-	XYZIsExecutable bool
-	XYZProcess      gobpmn_hash.Injection
-}
-
-type TestEmbeddedTenant struct {
-	XYZStartEvent gobpmn_hash.Injection
-}
-
-func TestInject(t *testing.T) {
-	t.Log("TestInjectEmbedded")
-	var hash gobpmn_hash.Injection
-	p := hash.Inject(TestProcess{}).(TestProcess)
-	p.Def = core.NewDefinitions()
-	t.Logf("p: %#+v", p)
-}
-
-func TestInjectEmbedded(t *testing.T) {
-	t.Log("TestInjectEmbedded")
-	var hash gobpmn_hash.Injection
-	p := hash.Inject(TestEmbeddedProcess{}).(TestEmbeddedProcess)
-	p.Def = core.NewDefinitions()
-	t.Logf("p: %#+v", p)
+// TestInjectConfig tests the injection of a bool type
+// into the field IsExecutable of the struct TestProcess
+// and TestEmbeddedPool.
+// The field IsExecutable is set to false as default, but
+// if reflected the field is set to true.
+// In gobpmnHash, a Config consists of a reflected struct field
+// which has a bool type.
+// This test should pass if the field IsExecutable is expected
+// set to true or false
+func TestInjectConfig(t *testing.T) {
+	t.Run("p(TestProcess{}).IsExecutable expected true",
+		func(t *testing.T) {
+			hash := new(gobpmn_hash.Injection)
+			p := hash.Inject(TestProcess{}).(TestProcess)
+			if assert.NotNil(t, p) {
+				assert.Equal(t, bool(true), p.IsExecutable)
+			}
+		})
+	t.Run("p(TestProcess{}).IsExecutable expected false",
+		func(t *testing.T) {
+			hash := new(gobpmn_hash.Injection)
+			p := hash.Inject(TestProcess{}).(TestProcess)
+			if assert.NotNil(t, p) {
+				assert.NotEqual(t, bool(false), p.IsExecutable)
+			}
+		})
+	t.Run("p(TestEmbeddedProcess{}).IsExecutable expected true", func(t *testing.T) {
+		hash := new(gobpmn_hash.Injection)
+		p := hash.Inject(TestEmbeddedProcess{}).(TestEmbeddedProcess)
+		if assert.NotNil(t, p) {
+			assert.Equal(t, bool(true), p.XYZIsExecutable)
+		}
+	})
+	t.Run("p(TestEmbeddedProcess{}).IsExecutable expected false", func(t *testing.T) {
+		hash := new(gobpmn_hash.Injection)
+		p := hash.Inject(TestEmbeddedProcess{}).(TestEmbeddedProcess)
+		if assert.NotNil(t, p) {
+			assert.NotEqual(t, bool(false), p.XYZIsExecutable)
+		}
+	})
 }
 
 func BenchmarkInject(b *testing.B) {
-	var hash gobpmn_hash.Injection
 	for n := 0; n < b.N; n++ {
-		p := hash.Inject(TestProcess{}).(TestProcess)
-		p.Def = core.NewDefinitions()
-		b.Logf("p: %#+v", p)
+		hash := new(gobpmn_hash.Injection)
+		_ = hash.Inject(TestProcess{}).(TestProcess)
 	}
 }
 
 func BenchmarkInjectEmbedded(b *testing.B) {
-	var hash gobpmn_hash.Injection
 	for n := 0; n < b.N; n++ {
-		p := hash.Inject(TestEmbeddedProcess{}).(TestEmbeddedProcess)
-		p.Def = core.NewDefinitions()
-		b.Logf("p: %#+v", p)
+		hash := new(gobpmn_hash.Injection)
+		_ = hash.Inject(TestEmbeddedProcess{}).(TestEmbeddedProcess)
 	}
 }
